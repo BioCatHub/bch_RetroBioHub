28790eb6957f8b9b8d5a0076fa5963012fe16f95
willfinnigan <wjafinnigan@gmail.com>
2022-01-28 12:19:59 +0000

    adding example use via requests package

---
+++ test_cases.py
@@ -0,0 +1,26 @@
+import requests
+
+def test_retrobiohub_endpoint(url):
+    ''' Test the /retrobiohub post endpoint '''
+    url = url + '/retrobiohub'
+
+    test_data = [{"substrates": ["CC(N)c1ccccc1"],
+                  "products": ["CC(=O)c1ccccc1"],
+                  "cofactors": [["Amine"], ["Carbonyl"]],
+                  "enzyme": "TA",
+                  "reaction": "Secondary amine deamination",
+                  }]
+
+    response = requests.post(url, data={'body': test_data})
+    return response
+
+
+# run flask app first, then try these tests
+if __name__ == '__main__':
+    test_url = 'http://127.0.0.1:5000/'
+
+    response = test_retrobiohub_endpoint(test_url)
+    print(response.text)
+
+
+
